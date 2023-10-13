5389f5ea463dc1d0240f1219442bb47a67c8a7f0
Ivan Genik <IVGenik@rambler.ru>
2023-10-13 20:12:45 +0500

    Add files via upload

---
+++ math_stat/lib/stat.dart
@@ -0,0 +1,12 @@
+import 'package:matrix2d/matrix2d.dart';
+
+// statistical functions
+void matrix2dfunc() {
+  List list = [
+    [1, 2],
+    [1, 2]
+  ];
+  print(list);
+  print('list.shape = ${list.shape}');
+  print('list.runtimeType = ${list.runtimeType}');
+}
