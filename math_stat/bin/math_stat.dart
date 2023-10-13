1b5dc2506e4c34252c963b130a8c34058f866625
Ivan Genik <IVGenik@rambler.ru>
2023-10-13 20:12:12 +0500

    Add files via upload

--- math_stat/bin/math_stat.dart
+++ math_stat/bin/math_stat.dart
@@ -2,10 +2,15 @@
 import 'package:math_stat/dfile.dart' as dfile;
 import 'package:math_stat/dstring.dart' as dstring;
 import 'package:math_stat/math1.dart' as math1;
+import 'package:math_stat/stat.dart' as stat;
+
+// import 'package:matrix2d/matrix2d.dart';
 
 void main() {
   // math1.theMathLoc();
-  math1.theRandomLoc([false, true, true]);
+  // math1.theRandomLoc([false, true, true]);
   // print(true | false);
   // dstring.testSplit1();
+    
+  stat.matrix2dfunc();
 }
