4a4321a3337236a4b666dfbb1e36b78f854e7649
Ivan Genik <IVGenik@rambler.ru>
2023-10-13 17:59:14 +0500

    Add files via upload

--- math_stat/bin/math_stat.dart
+++ math_stat/bin/math_stat.dart
@@ -1,1 +1,11 @@
+// import 'dart:ffi';
+import 'package:math_stat/dfile.dart' as dfile;
+import 'package:math_stat/dstring.dart' as dstring;
+import 'package:math_stat/math1.dart' as math1;
 
+void main() {
+  // math1.theMathLoc();
+  math1.theRandomLoc([false, true, true]);
+  // print(true | false);
+  // dstring.testSplit1();
+}
